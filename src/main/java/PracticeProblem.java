f6ed6f1debc2ae0273fd92f04d62813ddc77bc0d
hailey-tam-ics4u1-03-25 <hailey.tam@student.tdsb.on.ca>
2025-03-04 23:08:11 +0000

    Completed Code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,33 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex(int arr[], int num){
+		try{
+			int value = arr[num];
+			return true;
+		}
+		catch (ArrayIndexOutOfBoundsException e){
+			return false;
+		}
+	}
 
+	public static int divide (int num1, int num2){
+		try{
+			int sum = num1/num2;
+			return sum;
+		}
+		catch (ArithmeticException e){
+			return 0;
+			}
+		}
+	
+		public static int safeConvertStringtoInt(String string){
+			try {
+			int num = Integer.parseInt(string);
+			return num;
+			}
+			catch (NumberFormatException e){
+			return 0;
+			}
+		}
 }
